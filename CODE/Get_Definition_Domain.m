e2da4be946325691eb080d0ea7eb031cc57001f4
Richard Anthony Fernandes <55104357+rfernand387@users.noreply.github.com>
2021-02-08 18:44:38 -0500

    Add files via upload

--- CODE/Get_Definition_Domain.m
+++ CODE/Get_Definition_Domain.m
@@ -38,13 +38,13 @@ switch N
         domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^4)+i7*(10^6);    
     case 8
         [i1,i2,i3,i4,i5,i6,i7,i8] = ind2sub(size(count),find(count>0));    
-        domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^4)+i7*(10^6)+i8*(10^7);   
+        domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^5)+i7*(10^6)+i8*(10^7);   
     case 9
         [i1,i2,i3,i4,i5,i6,i7,i8,i9] = ind2sub(size(count),find(count>0));    
-        domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^4)+i7*(10^6)+i8*(10^7)+(i9*10^8);  
+        domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^5)+i7*(10^6)+i8*(10^7)+(i9*10^8);  
     case 10
         [i1,i2,i3,i4,i5,i6,i7,i8,i9,i10] = ind2sub(size(count),find(count>0));    
-        domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^4)+i7*(10^6)+i8*(10^7)+(i9*10^8)+i10*(10^9);   
+        domain = i1*10^0+i2*(10^1)+i3*(10^2)+i4*(10^3)+i5*(10^4)+i6*(10^5)+i7*(10^6)+i8*(10^7)+(i9*10^8)+i10*(10^9);   
     otherwise
          print("Truncating domain to first 8 input bands")
         [i1,i2,i3,i4,i5,i6,i7,i8,i9,i10] = ind2sub(size(count),find(count>0));    
